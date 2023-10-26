424a6d086f7ad0a1c24d9b24e2ed3e4e95855f92
Vladislav Ponomarev <farazeus@gmail.com>
2023-10-27 03:03:47 +0700

    0001 Two sum

---
+++ 0001_two_sum/0001_two_sum.go
@@ -0,0 +1,13 @@
+package leetcode
+
+// twoSum via bruteforce, -- O(n²) complexity, O(1) memory
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums) - 1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] + nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
